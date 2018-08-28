53c10de9299eadbe77687082a89d06f1efabcc53
桑超 <947934454@qq.com>
2018-08-28 21:40:41 +0800

    bug检查

--- zhuanQu/zhuanqu-product/js/zhuanqu-product-main.js
+++ zhuanQu/zhuanqu-product/js/zhuanqu-product-main.js
@@ -22,59 +22,28 @@ var zqMain = function () {
         });
     };
     var itemSelect=function () {
-        $('.js-address-Filter-a[tz-tm]').click(function () {
-            var tz= $(this).attr('tz-tm');
-        })
-        $.ajax({
-            type:'post',
-            data:tz,
-            dataType:'json',
-            url:url,
-            success:function () {
-                $.getJSON(url,data,function () {
-                    $data.each(function () {
-                        var id="   <a class=\"recommend-product-a  mag20\" href=\"zhuanqu-product/index2.html\">\n" +
-                            "                    <div class=\"recommend-product-list\">\n" +
-                            "                        <div class=\"recommend-product-list-top\">\n" +
-                            "                            <s class=\"recommend-product-list-a\"></s>\n" +
-                            "                            <p class=\"recommend-product-p\">首投，民营</p>\n" +
-                            "                        </div>\n" +
-                            "                        <table class=\"recommend-product-list-table\">\n" +
-                            "                            <tr class=\"b\">\n" +
-                            "                                <td>起投额</td>\n" +
-                            "                                <td>最高限额</td>\n" +
-                            "                            </tr>\n" +
-                            "                            <tr class=\"color\">\n" +
-                            "                                <td>100元</td>\n" +
-                            "                                <td>10000元</td>\n" +
-                            "                            </tr>\n" +
-                            "                            <tr class=\"b\">\n" +
-                            "                                <td>小猪返利综合年化</td>\n" +
-                            "                                <td>平台原年化</td>\n" +
-                            "                            </tr>\n" +
-                            "                            <tr class=\"color\">\n" +
-                            "                                <td class=\"color-num\">0.98%</td>\n" +
-                            "                                <td>0.8%</td>\n" +
-                            "                            </tr>\n" +
-                            "                            <tr class=\"b\">\n" +
-                            "                                <td>标期</td>\n" +
-                            "                                <td>风险评估</td>\n" +
-                            "                            </tr>\n" +
-                            "                            <tr class=\"color\">\n" +
-                            "                                <td>1天</td>\n" +
-                            "                                <td>89</td>\n" +
-                            "                            </tr>\n" +
-                            "                        </table>\n" +
-                            "                        <button>立即投资</button>\n" +
-                            "                    </div>\n" +
-                            "                </a>"
-                        var temp=template(id,data);
-                       $('.recommend-product').append(temp);
-                    });
-                    //todo
-                });
-                
-            }
+      var loadprd=  function(){
+           var proType = $('.js-address-Filter-a[pro-type].on').attr('pro-type');
+            var tcBak = $('.js-address-Filter-a[tc-bak].on').attr('tc-bak');
+            var tzTm = $('.js-address-Filter-a[tz-tm].on').attr('tz-tm');
+            var fx = $('.js-address-Filter-a[fx].on').attr('fx');
+            
+          $.ajax({
+              type:'post',
+              data:{proType:proType,tcBak:tcBak,tzTm:tzTm,fx:fx},
+              dataType:'json',
+              url:url,
+              success:function (data) {
+                      data.forEach(function (item,index) {
+                       var div="<div><p></p></div>";
+                          $('.recommend-product').append(div);
+                  });
+              }
+          })
+        }
+        
+        $('.js-address-Filter-a').click(function () {
+            loadprd();
         })
     }
     return{
